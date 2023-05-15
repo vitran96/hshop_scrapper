6d0ad39012a19f5df6589da0513d762d7a1eaee6
vitran96 <tranuytrieuvi@gmail.com>
2023-05-15 07:43:58 +0700

    update example with Vec<Person> to Vec<Element> and convert to Col

--- choose_game/src/main.rs
+++ choose_game/src/main.rs
@@ -1,25 +1,49 @@
-use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Sandbox, Settings};
+use iced::widget::{button, column, container, row, scrollable, text, Column, Scrollable, Text};
+use iced::{Alignment, Element, Renderer, Sandbox, Settings};
+use iced_native::Length;
+use serde::{Deserialize, Serialize};
 
 pub fn main() -> iced::Result {
-    Counter::run(Settings::default())
+    ListPerson::run(Settings::default())
 }
 
-struct Counter {
-    value: i32,
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+struct Person {
+    pub name: String,
+    pub age: i32,
+}
+
+impl Person {
+    pub(crate) fn view(&self) -> Element<Message> {
+        row![text(self.name.clone()), text(self.age.to_string())]
+            .spacing(8)
+            .into()
+    }
+}
+
+struct ListPerson {
+    persons: Vec<Person>,
 }
 
 #[derive(Debug, Clone, Copy)]
-enum Message {
-    IncrementPressed,
-    DecrementPressed,
-}
+enum Message {}
 
-impl Sandbox for Counter {
+impl Sandbox for ListPerson {
     type Message = Message;
 
     fn new() -> Self {
-        Self { value: 0 }
+        Self {
+            persons: vec![
+                Person {
+                    name: "Alice".to_string(),
+                    age: 32,
+                },
+                Person {
+                    name: "Bob".to_string(),
+                    age: 42,
+                },
+            ],
+        }
     }
 
     fn title(&self) -> String {
@@ -27,24 +51,20 @@ impl Sandbox for Counter {
     }
 
     fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 1;
-            }
-            Message::DecrementPressed => {
-                self.value -= 1;
-            }
-        }
+        match message {}
     }
 
     fn view(&self) -> Element<Message> {
-        column![
-            button("Increment").on_press(Message::IncrementPressed),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::DecrementPressed)
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
+        let list = self
+            .persons
+            .iter()
+            .map(|p| p.view())
+            .collect::<Vec<Element<Message>>>();
+
+        let col = Column::with_children(list);
+
+        let scrollable = scrollable(container(col).width(Length::Fill));
+
+        container(scrollable).into()
     }
 }
